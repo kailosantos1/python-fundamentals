677b7fdd2a7b079ec0ecd4decc7dbc110fd96e42
Kailo Santos <105285640+kailosantos1@users.noreply.github.com>
2026-08-25 00:13:08 -0300

    Update 25.py

--- Exercicios/Modulo_2/25.py
+++ Exercicios/Modulo_2/25.py
@@ -1,32 +1,30 @@
 verificador = True
 c = 0
 a = 0
-comparador = 0
 media = 0
-maior = 0
-menor = 0
 
 while verificador == True:
     num = float(input("Digite um numero: "))
 
-    if comparador != num:
-        if comparador < num and maior < num:
-                maior = num
-        if comparador > num and menor > num:
-           menor = num
-
-    comparador = num   
+    if a == 0:
+        maior = num
+        menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+ 
     c += num
-    a += 1
+    a += 1     
 
-    print(comparador)    
-    print(maior)
-    print(menor)   
-#     escolha = str(input("Deseja continuar (S/N)? ")).upper()
-#     if escolha == "N":
-#         verificador = False
+    escolha = str(input("Deseja continuar (S/N)? ")).upper()
+    if escolha == "N":
+        verificador = False
 
 
 
-# media = c / a
-# print(f"A media e: {media}")
+media = c / a
+print(f"A media e: {media}")
+print(f"O maior numero digitado foi: {maior}")
+print(f"O menor numero digitado foi: {menor}")
